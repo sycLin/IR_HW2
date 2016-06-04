e1c9f71be99a09722448f2a721ae55b2509d7a03
sycLin <b01902044@ntu.edu.tw>
2016-06-05 02:17:52 +0800

    + EM algorithm done

--- hw2_em.py
+++ hw2_em.py
@@ -5,6 +5,22 @@ from copy import deepcopy # for EM algorithm
 from hw2 import Corpus, Category, Document # import my classes from hw2.py
 from hw2 import ErrorExit, extractWordsFromFile # import my functions from hw2.py
 
+def converge(newResult, oldResult):
+	"""
+	here is my own definition of convergence.
+	for each document:
+	    c_new == c_old, and
+	    abs(est_new - est_old) <= 100
+	"""
+	# key: documentName, value: (cName, estimation)
+	for dname, detail in oldResult.iteritems():
+		try:
+			if detail[0] != newResult[dname][0] or abs(newResult[dname][1] / detail[1] - 1.0) > 0.001:
+				return False
+		except:
+			return False
+	return True
+
 def main():
 	# check argument count
 	if len(sys.argv) != 4:
@@ -39,12 +55,46 @@ def main():
 	unlabelDocs = {} # key: docName, value: words
 	for dname in os.listdir(unlabelDir):
 		unlabelDocs[dname] = extractWordsFromFile(os.path.join(unlabelDir, dname))
+
 	# classify unlabelDocs
-	result = {}
+	result = {} # key: documentName, value: (cName, estimation)
 	for dname, words in unlabelDocs.iteritems():
 		result[dname] = myCorpus.classify(words)
 	# add to myCorpus as tmpCorpus
 	tmpCorpus = deepcopy(myCorpus)
+	for dname, words in unlabelDocs.iteritems():
+		cname = result[dname][0]
+		tmpCorpus.buildVocabulary(cname, dname, words)
+	# classify with tmpCorpus
+	tmp_result = {} # key: documentName, value: (cName, estimation)
+	for dname, words in unlabelDocs.iteritems():
+		tmp_result[dname] = tmpCorpus.classify(words)
+	# main while loop
+	iter_counter = 1
+	while not converge(newResult=tmp_result, oldResult=result):
+		# record the last result in "result"
+		result = deepcopy(tmp_result)
+		# tmpCorpus <- myCorpus + result
+		tmpCorpus = deepcopy(myCorpus)
+		for dname, words in unlabelDocs.iteritems():
+			cname = result[dname][0]
+			tmpCorpus.buildVocabulary(cname, dname, words)
+		# classify unlabelDocs with tmpCorpus => tmp_result
+		tmp_result = {}
+		for dname, words in unlabelDocs.iteritems():
+			tmp_result[dname] = tmpCorpus.classify(words)
+		iter_counter += 1
+		print >> sys.stderr, "iteration #%d" % (iter_counter)
+	print >> sys.stderr, "total iteration count = %d" % (iter_counter)
+
+
+	# finish EM, and tmpCorpus is with adjusted parameters
+	# classify the test data
+	myCorpus = tmpCorpus
+	for i in range(1, 9420):
+		print "%d %s" % (i, myCorpus.classify(extractWordsFromFile(os.path.join(testDir, str(i))))[0])
+
+
 	pass
 
 
